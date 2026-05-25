1686b879aa75060ac102a786e51f7ce33ed2268e
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-26 00:33:02 +0300

    Update RightHandConfig.java

--- src/main/java/com/example/mymod/RightHandConfig.java
+++ src/main/java/com/example/mymod/RightHandConfig.java
@@ -4,4 +4,7 @@ public class RightHandConfig {
     public static float rightX = 0.12f; 
     public static float rightY = 0.10f;
     public static float rightZ = -0.45f;
+    
+    // Режим анимации удара: от 0 до 6 (всего 7 вариантов)
+    public static int swingMode = 0; 
 }
