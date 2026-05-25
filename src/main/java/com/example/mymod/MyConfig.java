70ea3ba018e16d3de5afe0ef5db318a32173354d
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-25 22:39:55 +0300

    Update MyConfig.java

--- src/main/java/com/example/mymod/MyConfig.java
+++ src/main/java/com/example/mymod/MyConfig.java
@@ -1,9 +1,11 @@
 package com.example.mymod;
 
 public class MyConfig {
+    // Координаты ЛЕВОЙ руки
     public static float leftY = 0.10f;
     public static float leftZ = -0.45f;
 
+    // Координаты ПРАВОЙ руки
     public static float rightY = 0.10f;
     public static float rightZ = -0.45f;
 }
