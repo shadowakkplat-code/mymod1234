b65564ae40f402137dceff761467e2b366508960
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-25 22:09:00 +0300

    Create LeftHandConfig.java

---
+++ src/main/java/com/example/mymod/LeftHandConfig.java
@@ -0,0 +1,7 @@
+package com.example.mymod.left;
+
+public class LeftHandConfig {
+    // Начальные координаты левой руки
+    public static float leftY = 0.10f;
+    public static float leftZ = -0.45f;
+}
