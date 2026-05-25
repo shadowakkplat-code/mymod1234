4b3a2abed6030a5129aa66004b8339a300a84082
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-26 00:06:51 +0300

    Update RightHandConfig.java

--- src/main/java/com/example/mymod/RightHandConfig.java
+++ src/main/java/com/example/mymod/RightHandConfig.java
@@ -1,7 +1,7 @@
 package com.example.mymod;
 
 public class RightHandConfig {
-    public static float rightX = 0.12f; // Начальное положение по горизонтали
+    public static float rightX = 0.12f; 
     public static float rightY = 0.10f;
     public static float rightZ = -0.45f;
 }
