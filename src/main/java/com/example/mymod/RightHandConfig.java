dced1f8196897a1709fd70a092ae16bd349ace22
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-25 22:09:14 +0300

    Create RightHandConfig.java

---
+++ src/main/java/com/example/mymod/RightHandConfig.java
@@ -0,0 +1,7 @@
+package com.example.mymod.right;
+
+public class RightHandConfig {
+    // Начальные координаты правой руки
+    public static float rightY = 0.10f;
+    public static float rightZ = -0.45f;
+}
