2cc371398684f2470abcdb43b2b9509b3f4d41bc
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-26 10:52:24 +0300

    Update RightHandConfig.java

--- src/main/java/com/example/mymod/RightHandConfig.java
+++ src/main/java/com/example/mymod/RightHandConfig.java
@@ -1,10 +1,19 @@
 package com.example.mymod;
 
 public class RightHandConfig {
+    // Координаты ПРАВОЙ руки
     public static float rightX = 0.12f; 
     public static float rightY = 0.10f;
     public static float rightZ = -0.45f;
+
+    // Координаты ЛЕВОЙ руки (Теперь снова здесь)
+    public static float leftX = -0.315f; 
+    public static float leftY = 0.10f;
+    public static float leftZ = -0.45f;
     
-    // Режим анимации удара: от 0 до 4 (всего 5 вариантов)
+    // Режим анимации удара: 0 - Ванилла, 1 - Прокрут 360
     public static int swingMode = 0; 
+
+    // Индекс выбранного PvP-эффекта частиц (от 0 до 4)
+    public static int particleMode = 0;
 }
