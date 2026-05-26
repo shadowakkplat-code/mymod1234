1576627f92ae4912cc81ab02decaa7c4e98a53c6
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-26 13:30:17 +0300

    Delete src/main/java/com/example/mymod/LeftHandConfig.java

--- src/main/java/com/example/mymod/LeftHandConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.mymod;
-
-public class LeftHandConfig {
-    public static float leftX = -0.315f; 
-    public static float leftY = 0.10f;
-    public static float leftZ = -0.45f;
-}
