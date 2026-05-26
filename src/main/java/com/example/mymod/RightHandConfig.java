77c0db9f355362112838a80ca2767e0a8b139ecd
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-26 12:23:43 +0300

    Update RightHandConfig.java

--- src/main/java/com/example/mymod/RightHandConfig.java
+++ src/main/java/com/example/mymod/RightHandConfig.java
@@ -11,6 +11,10 @@ public class RightHandConfig {
     public static float leftY = 0.10f;
     public static float leftZ = -0.45f;
     
+    // Процент уменьшения рук (от 0 до 90). По умолчанию левая уменьшена на 50%, правая на 0%
+    public static int leftScalePercent = 50;
+    public static int rightScalePercent = 0;
+    
     // Режим анимации удара: 0 - Ванилла, 1 - Прокрут 360
     public static int swingMode = 0; 
 
