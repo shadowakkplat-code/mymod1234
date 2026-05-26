eadc4ff0f42769143e368ef749b07b2084c7f367
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-26 15:27:00 +0300

    Update RightHandConfig.java

--- src/main/java/com/example/mymod/RightHandConfig.java
+++ src/main/java/com/example/mymod/RightHandConfig.java
@@ -1,17 +1,17 @@
 package com.example.mymod;
 
 public class RightHandConfig {
-    // Координаты правой руки (Клавиша K)
+    // Настройки ПРАВОЙ руки (Клавиша K)
     public static float rightX = 0.12f; 
     public static float rightY = 0.10f;
     public static float rightZ = -0.45f;
-    public static int rightScalePercent = 0; // Уменьшение правой руки в %
+    public static int rightScalePercent = 30; // Изначально уменьшена на 30%
 
-    // Координаты левой руки (Клавиша I)
+    // Настройки ЛЕВОЙ руки (Клавиша I)
     public static float leftX = -0.315f; 
     public static float leftY = 0.10f;
     public static float leftZ = -0.45f;
-    public static int leftScalePercent = 50; // Уменьшение левой руки в %
+    public static int leftScalePercent = 50; // Изначально уменьшена на 50%
     
     public static int swingMode = 0; 
     public static int activeParticleId = 0;
